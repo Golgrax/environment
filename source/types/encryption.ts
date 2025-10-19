9ea087150bd86d0cf564f787a7c32d18c0719899
Striatp <sebastien.works@outlook.com>
2025-10-19 14:49:29 +0200

    feat(types): add typing for the environment ✨

---
+++ source/types/encryption.ts
@@ -0,0 +1,14 @@
+/**
+ * Represents the most commonly used SHA-based
+ * encryption and hashing algorithms in Node.js.
+ */
+export enum EncryptionAlgorithm {
+  SHA224 = "sha224",
+  SHA256 = "sha256",
+  SHA384 = "sha384",
+  SHA512 = "sha512",
+  SHA3_256 = "sha3-256",
+  SHA3_512 = "sha3-512",
+  SHAKE128 = "shake128",
+  SHAKE256 = "shake256",
+}
