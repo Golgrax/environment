27192899a3cca3dbcbb2e340b5c762ec0634c4b4
Striatp <sebastien.works@outlook.com>
2025-10-21 16:10:19 +0200

    feat(exports): export the UserEnvironment class and global types ✨

--- source/index.ts
+++ source/index.ts
@@ -4,6 +4,9 @@ export * from "@/classes/process/ProcessController";
 export * from "@/classes/process/ProcessRegistry";
 export * from "@/classes/process/ProcessMonitor";
 
+export * from "@/classes/user/UserEnvironment";
+
 // Types
 export * from "@/types/process";
 export * from "@/types/encryption";
+export * from "@/types/global";
