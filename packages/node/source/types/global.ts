f5f5fe1a7612fdfc23b867ee4a145f0eb6971a8d
Striatp <sebastien.works@outlook.com>
2025-10-24 22:54:44 +0200

    feat(types): add globally-shared types ✨

---
+++ packages/node/source/types/global.ts
@@ -0,0 +1,11 @@
+/**
+ * An enum representing the different supported OSs.
+ */
+export enum Platform {
+  /** Representing Windows */
+  Windows = "win32",
+  /** Representing MacOS */
+  MacOS = "darwin",
+  /** Representing Linux */
+  Linux = "linux",
+}
