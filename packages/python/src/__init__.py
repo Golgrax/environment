09d99516e91104d01aaaa5c9abec9976c0b469c3
Bekian <Bekian.Cat@gmail.com>
2025-11-06 21:31:50 -0600

    fix(app): fixed gitignore and other minor changes
    refactored python section of gitignore so it's a little more cohesive. fixed name in project.toml to be  instead of , and added project-dir folder. moved new files into their proper folders, updated parent __init__.py file to work with this change. fixed an issue in user_environment.py using an incorrect union return type.

---
+++ packages/python/src/__init__.py
@@ -0,0 +1,6 @@
+from .system import system_environment
+from .user import user_environment
+from .process import process_environment
+from .process import process
+
+__all__ = ["system_environment", "user_environment", "process_environment", "process"]
