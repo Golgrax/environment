0c12bb5e057863d12f1206ab38da7903c705b86c
Bekian <Bekian.Cat@gmail.com>
2025-11-15 16:15:44 -0600

    feat(app): added system_environment features
    see changelog for v0.0.7

--- packages/python/src/__init__.py
+++ packages/python/src/__init__.py
@@ -1,6 +1,4 @@
 from .system import system_environment
-from .user import user_environment
 from .process import process_environment
-from .process import process
 
-__all__ = ["system_environment", "user_environment", "process_environment", "process"]
+__all__ = ["system_environment", "process_environment"]
